609f2a2f718f293203205ff111775250b849cbd3
Kevin Shu <kevin78515@gmail.com>
2016-10-26 13:21:38 +0800

    Update phosphorus.js

--- phosphorus.js
+++ phosphorus.js
@@ -292,7 +292,7 @@ var P = (function() {
     var request = new CompositeRequest;
 
     request.defer = true;
-    request.add(P.IO.load('http://crossorigin.me/http://scratch.mit.edu/projects/' + id + '/').onLoad(function(data) {
+    request.add(P.IO.load('https://scratch.mit.edu/projects/' + id + '/').onLoad(function(data) {
       var m = /<title>\s*(.+?)(\s+on\s+Scratch)?\s*<\/title>/.exec(data);
       if (callback) request.onLoad(callback.bind(self));
       if (m) {
